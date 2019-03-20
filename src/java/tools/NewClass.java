640aa001c2f629b647a6deb79ec6802148a726a8
Pandu Galang <pandu4431@gmail.com>
2019-03-20 13:49:59 +0700

    n

--- src/java/tools/NewClass.java
+++ src/java/tools/NewClass.java
@@ -9,10 +9,12 @@ import controllers.EmployeeController;
 import controllers.EmployeeControllerInterface;
 import controllers.OvertimeController;
 import controllers.OvertimeControllerInterface;
+import java.nio.charset.Charset;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.Random;
 import models.Overtime;
 import org.hibernate.SessionFactory;
 
@@ -23,23 +25,29 @@ import org.hibernate.SessionFactory;
 public class NewClass {
 
     public static void main(String[] args) {
-        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+//        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 //        OvertimeControllerInterface oc = new OvertimeController(sessionFactory);
-        EmployeeControllerInterface ec = new EmployeeController(sessionFactory);
+//        EmployeeControllerInterface ec = new EmployeeController(sessionFactory);
 //        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 //
 //        String x = "2019/02/13";
 //        Date dt = new Date();
-        String password = "galang";
-        String password2 = "galangs";
-        String passwordHash = BCrypt.hashpw(password, BCrypt.gensalt());
-        System.out.println(passwordHash);
-        String passwordHash2 = BCrypt.hashpw(password, BCrypt.gensalt());
-        System.out.println(passwordHash2);
+//        String password = "galang";
+//        String password2 = "galangs";
+//        String passwordHash = BCrypt.hashpw(password, BCrypt.gensalt());
+//        System.out.println(passwordHash);
+//        String passwordHash2 = BCrypt.hashpw(password, BCrypt.gensalt());
+//        System.out.println(passwordHash2);
+//
+//        System.out.println(BCrypt.checkpw(password2, passwordHash));
+//        System.out.println(BCrypt.checkpw(password, passwordHash2));
 
-        System.out.println(BCrypt.checkpw(password2, passwordHash));
-        System.out.println(BCrypt.checkpw(password, passwordHash2));
-
+//        byte[] array = new byte[7]; // length is bounded by 7
+//        new Random().nextBytes(array);
+//        String generatedString = new String(array, Charset.forName("UTF-8"));
+//        System.out.println(generatedString);
+//      
+        
 //        System.out.println(oc.getById("EMP5"));
 //        System.out.println(oc.totOver("EMP01"));
 //        System.out.println(oc.update("OVT19", dt, "2", "NGANTUK NGGA SIH", "TSH7", "STA02"));
